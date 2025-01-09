125149171b1531a1080c2f63e279359fd0830345
Bishista Bajracharya <135949183+Bishista@users.noreply.github.com>
2025-01-09 16:49:41 +0545

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PursePal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PursePal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
